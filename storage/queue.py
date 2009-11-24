9191b1e18bd256e524595e7cf52648f3b6a5b254
Sam Tingleff <sam@hal.samnbree.net>
2009-11-23 22:42:47 -0800

    switched queue impl from a custom syncrhonzied dict to Queue.Queue.

--- storage/queue.py
+++ storage/queue.py
@@ -1,81 +1,64 @@
 import logging
+import Queue
 import time
 from threading import RLock
 
 class QueueFullException(Exception):
     pass
 
-class BoundedSynchronizedDictionary(dict):
+class BoundedList(object):
     def __init__(self, limit=100):
-        self.lock = RLock()
+        self.queue = Queue.Queue(limit)
         self.limit = limit
 
-    def __getitem__(self, key):
-        return dict.__getitem__(self, key)
-
-    def __setitem__(self, key, value):
-        if len(dict(self)) >= self.limit:
-            logging.error("memory queue size reached max of %s items. Rejecting set()" % self.limit)
-            raise QueueFullException
-        self.lock.acquire()
-        try: dict.__setitem__(self, key, value)
-        finally: self.lock.release()
-
-    def __delitem__(self, key):
-        self.lock.acquire()
-        try: dict.__delitem__(self, key)
-        finally: self.lock.release()
-
-    def copy_and_clear(self):
-        self.lock.acquire()
+    def push(self, value):
         try:
-            copy = dict.copy(self)
-            dict.clear(self)
-            return copy
-        finally: self.lock.release()
+            self.queue.put(value)
+        except Queue.Full: raise QueueFullException
+
+    def pop(self):
+        try: return self.queue.get(False)
+        except Queue.Empty: return None
 
 class QueuedStorage(object):
     def __init__(self, backend, limit=1000):
         self.backend = backend
-        self.queue = BoundedSynchronizedDictionary(limit=limit)
-        self.to_flush = {}
+        self.queue = BoundedList(limit=limit)
+        self.limit = limit
 
     def get(self, key):
-        try:
-            return self.queue[key][1]
-        except KeyError:
-            try: return self.to_flush[key][1]
-            except KeyError: return self.backend.get(key)
+        return self.backend.get(key)
 
     def set(self, key, value):
-        self.queue[key] = (self.backend.set_txn, value)
+        self.queue.push( (self.backend.set_txn, key, value) )
 
     def set_bulk(self, dict):
         for key in dict.keys(): self.set(key, dict[key])
 
     def delete(self, key):
-        try: del self.queue[key]
-        except KeyError: pass
-        self.queue[key] = (self.backend.delete_txn, None)
+        self.queue.push( (self.backend.delete_txn, key, None) )
 
     def delete_bulk(self, keys):
         self.delete(key)
 
     def flush(self):
         start = time.time()
-        self.to_flush = self.queue.copy_and_clear()
         txn = self.backend.create_txn()
+        flushed = 0
         try:
-            for key in self.to_flush.keys():
-                op, val = self.to_flush[key]
+            tuple = self.queue.pop()
+            while (tuple is not None) and flushed < self.limit:
+                op, key, val = tuple
                 op(key, val, txn)
+                flushed += 1
+                tuple = self.queue.pop()
             self.backend.commit_txn(txn)
         except Exception, e:
             logging.exception(e)
             self.backend.abort_txn(txn)
         self.backend.flush()
         duration = int(time.time() - start)
-        logging.info("flushed %d items in %s seconds" % (len(self.to_flush), duration))
+        logging.info("flushed %d items in %s seconds" % (flushed, duration))
         self.to_flush.clear()
 
     def sync(self): self.backend.sync()
