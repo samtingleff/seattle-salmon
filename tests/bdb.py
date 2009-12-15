f1b4b8d105cfc4f91e40a795339b075624c0003e
Sam Tingleff <sam@hal.samnbree.net>
2009-12-15 14:34:45 -0800

    test case for bdb storage

---
+++ tests/bdb.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os
+import sys
+import unittest
+
+sys.path.append("..")
+sys.path.append(os.path.join(sys.path[0], '..'))
+
+from config import TestConfig
+from storage.bdb import BTree
+
+class BTreeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.storage = BTree(TestConfig())
+        self.values = {'a':'1', 'b':'2', 'c':'3', 'd':'4', 'e':'5'}
+
+    def tearDown(self):
+        self.storage.close()
+
+    def testSetGetDelete(self):
+        for key in self.values.keys(): self.storage.set(key, self.values[key])
+        for key in self.values.keys(): self.assertEquals(self.values[key], self.storage.get(key))
+        for key in self.values.keys(): self.storage.delete(key)
+        for key in self.values.keys(): self.assertFalse(self.storage.get(key))
+
+    def testIterator(self):
+        for key in self.values.keys(): self.storage.set(key, self.values[key])
+        iter = self.storage.iterator()
+        found = {}
+        for key, value in iter:
+            found[key] = self.storage.get(key)
+        self.assertEquals(len(self.values), len(found))
+        self.assertEquals(self.values, found)
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ tests/config.py
@@ -0,0 +1,17 @@
+
+class TestConfig(object):
+    configs = {
+        'bdb':{
+            'data-dir':'/home/sam/src/seattle-salmon/tmp/data',
+            'log-dir':'/home/sam/src/seattle-salmon/tmp/logs',
+            'splits':8}
+    }
+
+    def get_option(self, category, name, default=None):
+        try: val = TestConfig.configs[category][name]
+        except KeyError: val = default
+        return val
+
+    def get_int_option(self, category, name, default=0):
+        return int(self.get_option(category, name, default=default))
+
