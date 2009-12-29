3e201fda90b9a1f1d8bb6b623670b9942ec607c0
Sam <sam@otto.samnbree.net>
2009-12-29 05:48:43 +0000

    use a system.configure callback for memcache plugin conf

--- plugins/system/memcache.py
+++ plugins/system/memcache.py
@@ -4,13 +4,14 @@ from cache import memcache
 
 class MemcachePlugin(IStoragePlugin):
     def __init__(self):
-        self._connect()
         PluginManager().register("storage.get", self.get)
         PluginManager().register("storage.set", self.set)
         PluginManager().register("storage.delete", self.delete)
+        PluginManager().register("system.configure", self.configure)
 
-    def _connect(self):
-        self.mc = memcache.Client(['localhost:11211'], debug=0)
+    def configure(self, func, config):
+        hosts = config.get_option('cache', 'memcached-hosts', 'localhost:11211').split(', ')
+        self.mc = memcache.Client(hosts, debug=0)
 
     def get(self, func, key):
         val = self.mc.get(key)
